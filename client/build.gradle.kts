907063a470357c5e7b57b95c8fcf4bc4bc9c0e1c
David Andlinger <david.andlinger@instant-it.at>
2025-12-20 13:14:49 +0100

    changed jupiter version to 5.14.1

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -24,8 +24,8 @@ dependencies {
     implementation("io.mikael:urlbuilder:2.0.9")
 
     testImplementation("org.wiremock:wiremock:3.13.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:6.0.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:6.0.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.14.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.14.1")
 }
 
 buildConfig {
